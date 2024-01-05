7fe491c2d67280f14a4e3e17ab1a3df98345b8ec
mateyoski <maciejnedza01@gmail.com>
2024-01-05 11:27:09 +0100

    "Changed js to a easier version found at benfowler04 github, learned, kinda copied"

--- script.js
+++ script.js
@@ -1,21 +1,54 @@
 let playerScore = 0
 let computerScore = 0
-let roundWinner = ''
+const buttons = document.querySelectorAll('input')
 
-function getRandomDraw() {
-    let randomNumber = Math.floor(Math.random()*3)
-    switch(randomNumber) {
-        case 0:
-            return 'ROCK'
-        case 1:
-            return 'PAPER'
-        case 2:
-            return 'SCISSORS'
-        
+
+function computerMove() {
+    let results = ['rock', 'paper', 'scissors']
+    return results[Math.floor(Math.random() * results.length)]
+}
+
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+function singleRound(playerSelection) {
+    let computerSelection = computerMove()
+    let result = ""
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') || 
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')){
+        playerScore += 1
+        result = ('You scored! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
+        }
     }
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+        + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+    }
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+        if (computerScore == 5) {
+               result += '<br><br>You lost the game buhahaha! Reload the page to play again'
+               disableButtons()
+        }
+    }
+    document.getElementById('result').innerHTML = result
+    return
 }
 
-function isGameOver() {
-    return playerScore === 5 || computerScore === 5
-}
-
+buttons.forEach(button => {
+    button.addEventListener('click', function (){
+        singleRound(button.value)
+    })
+})
